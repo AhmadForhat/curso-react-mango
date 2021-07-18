cf5f3bcbe0405d5110998d5a6ee2c4ecbf993ebd
AhmadForhat <ahmad.forhat@gmail.com>
2021-07-18 05:35:43 -0300

    feat: add router

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import { Login } from '@/presentation/pages'
+import Router from '@/presentation/router'
 
 import './global-styles/styles.scss'
 
 ReactDOM.render(
-  <Login />,
+  <Router />,
   document.getElementById('main')
 )
